cfcb9e36360c76a5771e0ff21d687e3c4c09c680
Yoru-cyber <carlosmendez170210@gmail.com>
2023-08-18 01:06:05 -0400

    las imagenes redireccionan, animacion al cargar la página, grid para las cartas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-import { MantineProvider, Title, Card, Image, Text } from "@mantine/core";
+import { MantineProvider, Title, Card, Image, Text, Spoiler, Grid, Center, Loader } from "@mantine/core";
 function App() {
   const [data, setData] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() =>{
+    setLoading(true);
+    setTimeout(()=>{
+      setLoading(false);
+    }, 2000);
+  }, []);
   useEffect(() => {
     fetch("https://api.jikan.moe/v4/top/anime?limit=10")
       .then((response) => response.json())
@@ -10,27 +17,36 @@ function App() {
   }, []);
   return (
     <>
+    {loading?(<><Center h={500}><Image src="https://gifdb.com/images/high/cute-levi-motion-chibi-artwork-eodsifvum00tzf3x.gif" style={{width: 200, height: 150}}></Image></Center><Text>Somebody has to clean</Text><Loader variant="dots" color="white"></Loader></>):(
       <MantineProvider theme={{colorScheme: 'dark'}}>
         <Title order={1}>Top Anime</Title>
-        <ul style={{ listStyle: "none" }}>
+        <Grid>
+        
           {data["data"]?.map((anime) => (
-            <li key={anime.mal_id}>
-              <Card shadow="sm" padding='lg' radius='lg' withBorder>
+            
+            <Grid.Col lg={6} xs={7}>
+              <Card style={{width: 500, margin: 20}} shadow="sm" padding='lg' radius='lg' withBorder>
                 <Title order={2}>
                   {anime.title}
                   <Title order={3}>Rank {anime.rank} </Title>
                 </Title>
-                <Image width='320' height='auto' mx='auto' radius='md' alt={anime.title} src={anime["images"]["jpg"].image_url}></Image>
+                <a target='_blank' href={anime.url}><Image width='320' height='auto' mx='auto' radius='md' alt={anime.title} src={anime["images"]["jpg"].image_url}></Image></a>
+                <Spoiler maxHeight={100} showLabel="Show more" hideLabel="Hide">
                 <Text>
                   {anime.synopsis}
                 </Text>
+              </Spoiler>
               </Card>
-            </li>
+              </Grid.Col>
+              
           ))}
-        </ul>
+         
+        </Grid>
       </MantineProvider>
+    )}
     </>
   );
 }
 //add quotes of each anime
+//custom list for user
 export default App;
